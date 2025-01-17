1f48b343349ccac69c8e925147507b0363ecd31c
srmro <833stephanie@gmail.com>
2025-01-17 01:10:53 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//create a single web application
+
+//need to access an external API
+
+
+//fetch the API to communicate with external web API
+
+//use data to populate your application's content and features
+
+
+//create user interactioin with API through a search feature(should) use get requests to retrieve associated data
+
+
+//enable user manipulation of data within the API through the use of POST,PUT,PATCH
+
+//ensure API suppports these features,
+
+
+//Make use of promises, asyn/await as appropiate
+
+
+//organize js code into atleast 3 different module files
+//and import functions and data acorss files as necessary
+
+
+//ensure the program runs as expected, without any undesired behavior cause
+//by misunderstanding of the js event loop
+
+
+
+//create engaging use experience through the use of HTML and CSS
+
+
+//Ensure program runs without errors.
+
+
+//commit frequently to git repository
